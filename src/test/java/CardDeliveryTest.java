23a3e5e5148551f90755c63281b469b6dad6b7fb
Jenek <Jenek619@gmail.com>
2023-08-22 09:28:29 +0300

    CardDeliveryTest2

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,24 +1,33 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
-import static com.codeborne.selenide.Selectors.withText;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+    public String Data(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     public void shouldCardDeliveryTest() {
         open("http://localhost:9999");
         $("method=post");
-        $("[type='text']").setValue("Нижний Новгород");
-        $("[type='tel']").setValue("25.08.2023");
-        $("[name='name']").setValue("Чистов Евгений");
-        $("[name='phone']").setValue("+79200000000");
+        $("[data-test-id='city'] input").setValue("Нижний Новгород");
+        String addDays = Data(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        $("[data-test-id='date'] input").sendKeys(addDays);
+        $("[data-test-id='name'] input").setValue("Чистов Евгений");
+        $("[data-test-id='phone'] input").setValue("+79012345678");
         $("[data-test-id='agreement']").click();
-        $("[role='button']").click();
-        $(withText("Успешно!")).shouldBe(Condition.exist);
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(20)).shouldHave(Condition.exactText("Встреча успешно забронирована на " + addDays));
     }
 
 }
